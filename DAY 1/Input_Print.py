8c26dda4a5ad1444d42299693739e78d4e45cc7a
KartikeyaGaur29 <65773042+KartikeyaGaur29@users.noreply.github.com>
2020-05-22 19:45:52 +0530

    Create Input_Print.py

---
+++ DAY 1/Input_Print.py
@@ -0,0 +1,11 @@
+name = input("Enter your name : ")
+branch = input("Enter your branch : ")
+gender = input("Enter your gender : ")
+college = input("Enter your college name : ")
+age = input("Enter your age : ")
+
+print("\n NAME :",name)
+print("\n BRANCH :",branch)
+print("\n GENDER :",gender)
+print("\n COLLEGE :",college)
+print("\n AGE :",age)
